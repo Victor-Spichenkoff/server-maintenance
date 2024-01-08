696ce30f300613d4f8c792cd60135bc63141a05a
Victor Spichenkoff <victorspich13@gmail.com>
2024-01-08 18:52:49 -0300

    Change Frequency

--- functions/schedule.ts
+++ functions/schedule.ts
@@ -1,6 +1,7 @@
 const schedule = require('node-schedule')
 import wrongUrls from './verify'
 import formatMensageAndSend from './sendToPhone'
+import axios from 'axios'
 
 var times = 0
 
@@ -9,6 +10,7 @@ async function verifyAndSend(sendMensage: boolean=false) {
 
     times++
 
+    if(sendMensage) axios.get('https://server-maintenance.onrender.com/load')
     if(sendMensage) formatMensageAndSend(objectWithWrong, times)
 }
 
@@ -16,16 +18,16 @@ async function verifyAndSend(sendMensage: boolean=false) {
 
 // Agendando a primeira execução
 const rule1 = {
-    hour: 20,
-    minute: 49,
-    second: 40    
+    // hour: 20,
+    minute: 30,
+    second: 0    
 }
 
 const job1 = schedule.scheduleJob(rule1, verifyAndSend)
 
 // Segunda execução (metodo 2)
 const rule2 = new schedule.RecurrenceRule()
-rule2.hour = 8
+// rule2.hour = 8
 rule2.minute = 0
 rule2.second = 0
 
